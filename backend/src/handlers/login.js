96617e4a2f4ad3907909c79fec43bb2a2a32526e
Mark Grebe <grebe@ucmo.edu>
2024-10-05 20:11:20 -0500

    Hadnle CORS for localhost testing

--- backend/src/handlers/login.js
+++ backend/src/handlers/login.js
@@ -19,18 +19,33 @@ module.exports.handler = async (event) => {
     if (!data.Item || data.Item.password !== requestBody.password) {
       return {
         statusCode: 401,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         body: JSON.stringify({ message: 'Invalid username or password' })
       };
     }
 
     return {
       statusCode: 200,
+      headers: {
+          "Access-Control-Allow-Headers" : "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+      },
       body: JSON.stringify({ message: 'Login successful' })
     };
   } catch (error) {
     console.error('Error:', error);
     return {
       statusCode: 500,
+      headers: {
+          "Access-Control-Allow-Headers" : "Content-Type",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+      },
       body: JSON.stringify({ message: 'Internal server error' })
     };
   }
